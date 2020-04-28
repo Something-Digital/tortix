4824a46b316a21fe4d7a1e6e5ba27c6c00790f45
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-04-28 19:00:22 +0500

    wip

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <h1 class="title">{{ title }}</h1>
+    <div class="search">Search</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
